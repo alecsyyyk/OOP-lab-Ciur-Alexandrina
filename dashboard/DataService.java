3b76b9ececa04ebc278cc4290da4676f11c426f5
Alexandrina Ciur <alexandrina.ciur@isa.utm.md>
2025-10-26 21:08:53 +0200

    Remove dashboard and unrelated files - focus on alien classification only

--- dashboard/DataService.java
+++
@@ -1,8 +0,0 @@
-public class DataService {
-    public void fetchLatest(AnalyticsDashboard dash) {
-         int deltaActive = (int) (Math.random() * 11); // 0..10
-        dash.setActiveUsers(dash.getActiveUsers() + deltaActive);
-        dash.setUserCount(Math.max(dash.getUserCount(), dash.getActiveUsers()));
-        dash.setRevenue(dash.getRevenue() + (float) (Math.random() * 200.0));
-    }
-}
