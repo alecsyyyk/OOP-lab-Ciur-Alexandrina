5a34922e4ff617c46a5046af63ab34405e0606e9
Alexandrina Ciur <alexandrina.ciur@isa.utm.md>
2025-10-26 21:05:05 +0200

    Day 1: Create AlienSpecies class and test program

---
+++ AlienSpecies.java
@@ -0,0 +1,27 @@
+﻿public class AlienSpecies {
+    private String name;
+    private String planet;
+    private String universe;
+
+    public AlienSpecies(String name, String planet, String universe) {
+        this.name = name;
+        this.planet = planet;
+        this.universe = universe;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPlanet() {
+        return planet;
+    }
+    
+    public String getUniverse() {
+        return universe;
+    }
+
+    public String toString() {
+        return name + " from planet " + planet + " (" + universe + " universe)";
+    }
+}
